a2c172cefc155fa0df2d5df52d4597d20b74d1cf
Florian Ludwig <f.ludwig@greyrook.com>
2018-09-18 22:11:08 +0200

    clone and check

--- cde/config.py
+++ cde/config.py
@@ -1,10 +1,28 @@
 import os
-import yaml
+import re
+from pathlib import Path
+
+from ruamel.yaml import YAML
 
 
-def get_config():
-    if os.path.exists('.cde.yml'):
-        with open('.cde.yml') as f:
-            return yaml.load(f)
+def validate(cfg):
+    ok = True
+    repo_name_re = '^[a-z][a-z0-9-]*$'
+    repo_name = re.compile(repo_name_re)
+    for name, repo in cfg['repos'].items():
+        if not repo_name.match(name):
+            msg = 'repo-keys must be {} but {} not'
+            print(msg.format(repo_name_re[1:-1], repr(name)))
+            ok = False
 
-    return {}
+    return ok
+
+
+def load(path=None):
+    if path is None:
+        path = Path('.') / Path('.cde.yml')
+
+    yaml = YAML(typ='safe')
+    yaml.allow_duplicate_keys = False
+    yaml.default_flow_style = False
+    return yaml.load(open(path))
--- setup.py
+++ setup.py
@@ -11,25 +11,19 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
+# TODO
 requirements = [
-    'Click>=6.0',
-    'pyyaml',
-    'docker',
-    'dockerpty',
+    "Click>=6.0",
+    "ruamel.yaml",
+    "docker",
+    "dockerpty",
 ]
-
-setup_requirements = [
-    'pytest-runner',
-]
-
-test_requirements = [
-    'pytest',
-]
+test_requirements = []
 
 setup(
-    name='cde',
+    name='cde-cli',
     version='0.1.0',
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    description="cli helper",
     long_description=readme + '\n\n' + history,
     author="Florian Ludwig",
     author_email='f.ludwig@greyrook.com',
@@ -50,14 +44,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements,
-    setup_requires=setup_requirements,
 )
