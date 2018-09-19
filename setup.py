dba4a1496732caa18430415496d1ee03e3ba16ac
Florian Ludwig <f.ludwig@greyrook.com>
2018-09-19 16:23:58 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = []
 
 setup(
     name='cde-cli',
-    version='0.1.0',
+    version='0.1.1',
     description="cli helper",
     long_description=readme,
     author="Florian Ludwig",
