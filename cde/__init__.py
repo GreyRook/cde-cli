1655faf5301fd2c827b5f005f3e745ebffd1e46a
Florian Ludwig <f.ludwig@greyrook.com>
2019-03-08 16:43:19 +0100

    Bump version: 0.1.1 → 0.2.0

--- cde/__init__.py
+++ cde/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Florian Ludwig"""
 __email__ = 'f.ludwig@greyrook.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = []
 
 setup(
     name='cde-cli',
-    version='0.1.1',
+    version='0.2.0',
     description="cli helper",
     long_description=readme,
     author="Florian Ludwig",
