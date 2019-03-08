87cebc622ad94921f2bee81d384d3605f1ba062d
Florian Ludwig <f.ludwig@greyrook.com>
2019-03-08 16:43:00 +0100

    refactoring

--- cde/config.py
+++ cde/config.py
@@ -4,12 +4,17 @@ from pathlib import Path
 
 from ruamel.yaml import YAML
 
+DEFAULT_CFG_PATH = Path('.') / Path('.cde.yml')
+yaml = YAML(typ='safe')
+yaml.allow_duplicate_keys = False
+yaml.default_flow_style = False
+
 
 def validate(cfg):
     ok = True
     repo_name_re = '^[a-z][a-z0-9-]*$'
     repo_name = re.compile(repo_name_re)
-    for name, repo in cfg['repos'].items():
+    for name, repo in cfg.get('repos', {}).items():
         if not repo_name.match(name):
             msg = 'repo-keys must be {} but {} not'
             print(msg.format(repo_name_re[1:-1], repr(name)))
@@ -20,11 +25,18 @@ def validate(cfg):
 
 def load(path=None):
     if path is None:
-        path = Path('.') / Path('.cde.yml')
+        path = DEFAULT_CFG_PATH
+    
+    if not os.path.exists(path):
+        return {}
 
-    yaml = YAML(typ='safe')
-    yaml.allow_duplicate_keys = False
-    yaml.default_flow_style = False
     data = yaml.load(open(path))
     data.setdefault('repos', {})
     return data
+
+
+def store(cfg, path=None):
+    if path is None:
+        path = DEFAULT_CFG_PATH
+
+    yaml.dump(cfg, path)
